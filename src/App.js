dcce61ff4076ada5d3ec6330e64a923f92cefd7e
unknown <loganathandiroshan.com>
2024-01-12 09:36:45 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import RegistrationForm from './RegistrationForm';
-
+import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Weather from './weather';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
-  
-      <RegistrationForm />
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/weather" element={<Weather />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
